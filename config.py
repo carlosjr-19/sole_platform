c12df4a7c58a03068a11ba23483d062895a6b2d4
carlosjr-19 <carlosvillarroeljr@hotmail.com>
2025-07-16 16:37:22 -0600

    Modulo comisiones reporte activaciones

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
 
+# Directorio raíz del proyecto
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "clave_por_defecto")
+
+    # Rutas personalizadas
+    UPLOAD_FOLDER = os.path.join(PROJECT_ROOT, 'sole_platform', 'static', 'storage')
+    DOWNLOAD_FOLDER = os.path.join(PROJECT_ROOT, 'sole_platform', 'static', 'downloads')
+    IMAGES_FOLDER = os.path.join(PROJECT_ROOT, 'sole_platform', 'static', 'img')
+
--- sole_platform/routes.py
+++ sole_platform/routes.py
@@ -1,4 +1,10 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request, jsonify, send_file
+from flask import current_app
+from .services.commissions import report_act as ra
+from .services.commissions import clean_folders as clean
+import polars as pl
+import pandas as pd
+import os
 
 def init_app(app):
 
@@ -21,3 +27,110 @@ def init_app(app):
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('error_404.html'), 404
+
+    @app.route('/commissions', methods = ['POST'])
+    def form_comisiones():
+        if request.method == 'POST':
+
+            UPLOAD_FOLDER = current_app.config['UPLOAD_FOLDER']
+
+            # Obtener archivos
+            csv_finanzas = request.files['file_csv']
+            xlsx_general = request.files['file_xlsx']
+
+            # Obtener datos del formulario
+            comision_sales = request.form.get('comision-sales')
+            proceso = request.form.get('proceso')
+            porcentaje = request.form.get('comision')
+            fecha = request.form['fecha']
+
+            print(f"Comision Sales: {comision_sales}, Proceso: {proceso}, Porcentaje: {porcentaje}, Fecha: {fecha}")
+
+            print(f"carpeta:  {UPLOAD_FOLDER}")
+
+            path_1 = os.path.join(UPLOAD_FOLDER, csv_finanzas.filename)
+            path_2 = os.path.join(UPLOAD_FOLDER, xlsx_general.filename)
+
+            csv_finanzas.save(path_1)
+            xlsx_general.save(path_2)
+
+            
+            # Leer archivos con polars
+            csv = pl.read_csv(path_1)
+            xlsx = pl.read_excel(path_2)
+
+            # verificar la marca del archivo de finanzas
+            valor = csv.select(pl.col("mvno_name").unique()).to_series().to_list()
+            print(f"Marca del archivo de finanzas: {valor[0]}.")
+
+            marca = valor[0]
+
+            clean.limpiar_storage()
+            clean.limpiar_downloads()
+
+            if proceso == "activacion":
+
+                
+                if marca == "Sigma Móvil ":
+                    marca = "Sigma MÃ³vil "
+                    print(f"Entro marca sigma")
+                elif marca == "Gou! Móvil":
+                    marca = "Gou! MÃ³vil"
+                elif marca == "Hey Móvil":
+                    marca = "Hey MÃ³vil"
+                
+
+                # Procesar los archivos
+                duplicados, csv_limpio = ra.limpiar_duplicados(csv)
+
+                #obtener los números duplicados
+                duplicados_list = duplicados.select("msisdn").to_series().to_list()
+
+                # Mostrar activaciones de la marca elegida
+                act_general = (
+                            xlsx.filter(pl.col("mvno_name") == marca)
+                            .select(pl.count())
+                            .item()
+                        )
+
+                csv_procesado, precios_iguales = ra.procesar_comisiones(csv_limpio, comision_sales, porcentaje, fecha)
+                print(csv_procesado)
+                df_pandas = csv_procesado.to_pandas()
+
+                # Separar totales
+                df_sin_total = df_pandas[df_pandas['mvno_package_name'] != 'TOTAL'].sort_values(by='date')
+                fila_total = df_pandas[df_pandas['mvno_package_name'] == 'TOTAL']
+
+                # Concatenar ordenados
+                df_pandas = pd.concat([df_sin_total, fila_total], ignore_index=True)
+
+
+                nombre_archivo = ra.estilos_excel(df_pandas, marca, precios_iguales, fecha)
+
+                print(f"Archivo generado: {nombre_archivo}")
+
+            return jsonify(
+                csv_finanzas=csv_finanzas.filename,
+                xlsx_general=xlsx_general.filename,
+                marca=marca,
+                comision_sales=comision_sales,
+                proceso=proceso,
+                porcentaje=porcentaje,
+                fecha=fecha,
+                num_duplicados=duplicados.height,
+                csv_limpio=csv_limpio.height,
+                total_general=act_general,
+                lista_duplicados=duplicados_list,
+                archivo_generado=nombre_archivo,
+            )
+        
+    @app.route('/descargar/<archivo>')
+    def descargar(archivo):
+        if os.environ.get("RAILWAY_ENVIRONMENT"):
+            ruta_archivo = os.path.join("/tmp", archivo)
+        else:
+            ruta_archivo = os.path.join("static", "downloads", archivo)
+
+        print(f"[DEBUG] Intentando descargar: {ruta_archivo}")
+
+        return send_file(ruta_archivo, as_attachment=True)
